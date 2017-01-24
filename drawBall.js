2c79af34c257b40b17d989d125747939f5392a6f
crsln <arscem@gmail.com>
2017-01-24 16:03:06 +0200

    Delete drawBall.js

--- drawBall.js
+++
@@ -1,9 +0,0 @@
-var ballRadius = 10;
-
-function drawBall(posX, posY){	
-		ctx.beginPath();
-		ctx.arc(posX, posY, ballRadius, 0, Math.PI*2);
-		ctx.fillStyle = color;
-		ctx.fill();
-		ctx.closePath();
-	}
