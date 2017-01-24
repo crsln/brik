50b42f3698f0aa76e0d56266e8ac605987e791d1
crsln <arscem@gmail.com>
2017-01-24 16:02:45 +0200

    Delete drawScore.js

--- drawScore.js
+++
@@ -1,9 +0,0 @@
-var score = 0;
-
-
-function drawScore(posX, posY){
-	ctx.font = "10px Arial";
-	ctx.fillStyle = "black";
-	ctx.fillText("Score: "+score, posX, posY);
-	document.getElementById('score').innerHTML = "SCORE: "+score;
-}
